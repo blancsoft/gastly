8ac89c25a44d10a7939cb70392106171671c246c
Chuma Umenze <git@chumaumenze.com>
2023-08-23 19:10:39 +0100

    Remove PWA and isolate interop lib

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -6,30 +6,14 @@ import (
 	"fmt"
 	"syscall/js"
 
-	"github.com/blancsoft/gastly/ast"
+	"github.com/blancsoft/gastly/js/objects"
 )
 
 func main() {
-	gastly := js.ValueOf(map[string]any{
-		"FromSourceCode": js.FuncOf(func(this js.Value, args []js.Value) any {
-			if len(args) < 2 {
-				return fmt.Errorf("FromSourceCode: missing required arguments — expects two arguments")
-			}
-			fname := args[0].String()
-			code := args[1].String()
-			result := ast.FromSourceCode(fname, code)
-			return ast.ConvertToJSValue(result)
-		}),
-		"FromPackages": js.FuncOf(func(this js.Value, args []js.Value) any {
-			var pkgNames []string
-			for _, pn := range args {
-				pkgNames = append(pkgNames, pn.String())
-			}
-			result := ast.FromPackages(pkgNames...)
-			return ast.ConvertToJSValue(result)
-		}),
-	})
-	js.Global().Set("Gastly", gastly)
+	js.Global().Set("Gastly", js.ValueOf(map[string]any{
+		"FromSourceCode": js.FuncOf(objects.FromSourceCode),
+		"FromPackages":   js.FuncOf(objects.FromPackages),
+	}))
 
 	for {
 		quitChannel := make(chan *struct{})
